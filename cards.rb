141d3e6f623634b069615be5e141df68afd9c20d
Alan Maciel <alan@ubernetics.com>
2015-03-03 12:47:08 -0600

    Using hash.each with  k and v

--- cards.rb
+++ cards.rb
@@ -1,4 +1,9 @@
-cards = ["Jack", "Queen", "King", "Ace", "Jocker"]
+# cards = ["Jack", "Queen", "King", "Ace", "Jocker"]
+# 
+# cards.reverse_each { |c| puts "#{c.upcase} - #{c.length}" }
+# 
+scores = {"Larry" => 10, "Moe" => 8, "Curly" => 12}
 
-cards.reverse_each { |c| puts "#{c.upcase} - #{c.length}" }
-
+scores.each do |k, v|
+  puts "#{k} scored a #{v}!"
+end
